fb2cd50b448d21a13a56fa9a54c84a9023157b2f
stefanMadzharov <83451593+stefanMadzharov@users.noreply.github.com>
2025-06-22 00:37:30 +0000

    Using criterion

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,17 +1,26 @@
+use criterion::{criterion_group, criterion_main, Criterion};
 use order_processor::runs;
+use pprof::criterion::{Output, PProfProfiler};
 
-fn main() {
-    let guard = pprof::ProfilerGuard::new(100).unwrap();
-
-    #[cfg(all(feature = "error_handling", not(feature = "no_inferring")))]
-    runs::run_inferring();
-    #[cfg(all(feature = "error_handling", feature = "no_inferring"))]
-    runs::run_no_inferring();
-    #[cfg(not(feature = "error_handling"))]
-    runs::run_optimized();
-    if let Ok(report) = guard.report().build() {
-        let file = std::fs::File::create("flamegraph.svg").unwrap();
-        report.flamegraph(file).unwrap();
-        println!("{:?}", report.timing.duration);
-    }
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("run_with_inferring", |b| {
+        b.iter(|| {
+            #[cfg(all(feature = "error_handling", not(feature = "no_inferring")))]
+            runs::run_inferring();
+            #[cfg(all(feature = "error_handling", feature = "no_inferring"))]
+            runs::run_no_inferring();
+            #[cfg(not(feature = "error_handling"))]
+            runs::run_optimized();
+        });
+    });
 }
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default()
+        .sample_size(50)
+        .measurement_time(std::time::Duration::new(30,0))
+        .with_profiler(PProfProfiler::new(100, Output::Flamegraph(None)));
+    targets = criterion_benchmark
+}
+criterion_main!(benches);
